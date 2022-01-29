5a2c6d560a35ebe9a9f00d1a4b22a16c5fc47988
Frédéric FOISSEY <frederic.foissey@gmail.com>
2022-01-29 16:56:03 +0100

    fix

--- malloc_checker.sh
+++ malloc_checker.sh
@@ -7,14 +7,16 @@ BIN=
 LIB_NAME=malloc_wrapper.dylib
 SCRIPT_NAME=$0
 RETURN_VALUE=${SUCCESS}
-
+RED="\033[31m"
+GREEN="\033[32m"
+NC="\033[0m"
 SUCCESS=0
 FAILURE=1
 
 USAGE="\nusage:\t${SCRIPT_NAME} bin={value} limit={value} [loop={value}]\n"
 HELP="option description:\n\tbin: binary you want to test\n\tlimit: the number of calls to malloc that can succeed before malloc returns NULL\n\tloop: [optional parameter] -> use if you want to loop from 0 to limit (default: false)"
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
 	echo -e $USAGE >&2
 	echo -e $HELP >&2
 	exit $FAILURE
@@ -45,14 +47,14 @@ fi
 
 for i in $(seq ${START} 1 ${MALLOC_LIMIT})
 do
-	echo ---- MALLOC_LIMIT=${i} -----
+	echo ---- MALLOC_LIMIT = ${i} -----
 	${CC} -dynamiclib -D MALLOC_LIMIT=${i} -o ${LIB_NAME} malloc_wrapper.c
-	DYLD_INSERT_LIBRARIES=${LIB_NAME} DYLD_FORCE_FLAT_NAMESPACE=1 ${BIN} 1 > /dev/null
+	DYLD_INSERT_LIBRARIES=${LIB_NAME} DYLD_FORCE_FLAT_NAMESPACE=1 ${BIN} 1>/dev/null
 	if [ "$?" -gt "127" ]; then
-		echo -e "\033[31mKO\033[0m"
+		echo -e "${RED}KO${NC}"
 		RETURN_VALUE=${FAILURE}
 	else
-		echo -e "\033[32mOK\033[0m"
+		echo -e "${GREEN}OK${NC}"
 	fi
 	rm -f ${LIB_NAME}
 done
