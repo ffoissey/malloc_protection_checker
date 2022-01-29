ab2fcd0a8bdffc158595b5f6b681e3f5afdfcc36
Frédéric FOISSEY <frederic.foissey@gmail.com>
2022-01-29 16:37:01 +0100

    malloc_checker.sh + malloc_wrapper.c

---
+++ malloc_checker.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+MALLOC_LIMIT=1
+LOOP=0
+CC=clang
+BIN=
+LIB_NAME=malloc_wrapper.dylib
+SCRIPT_NAME=$0
+RETURN_VALUE=${SUCCESS}
+
+SUCCESS=0
+FAILURE=1
+
+USAGE="\nusage:\t${SCRIPT_NAME} bin={value} limit={value} [loop={value}]\n"
+HELP="option description:\n\tbin: binary you want to test\n\tlimit: the number of calls to malloc that can succeed before malloc returns NULL\n\tloop: [optional parameter] -> use if you want to loop from 0 to limit (default: false)"
+
+if [ -z $1 ]; then
+	echo -e $USAGE >&2
+	echo -e $HELP >&2
+	exit $FAILURE
+fi
+for ARG in "$@";
+do
+	if [[ ${ARG} == "loop="* ]]; then
+		LOOP=$(echo $ARG | cut -d '=' -f 2)
+	elif [[ ${ARG} == "limit="* ]]; then
+		MALLOC_LIMIT=$(echo $ARG | cut -d '=' -f 2)
+	elif [[ ${ARG} == "bin="* ]]; then
+		BIN=$(echo $ARG | cut -d '=' -f 2)
+	elif [ "${ARG}" == "--help" ] || [ "${ARG}" == "-h" ]; then
+		echo -e $USAGE >&2
+		echo -e $HELP >&2
+		exit $FAILURE
+	else
+		echo -e "${RED}Bad argument: ${ARG}${NC}\n${USAGE}" >&2
+		exit $FAILURE
+	fi
+done
+
+START=${MALLOC_LIMIT}
+
+if [[ ${LOOP} == "true" ]] || [[ ${LOOP} == "1" ]]; then
+	START=0
+fi
+
+for i in $(seq ${START} 1 ${MALLOC_LIMIT})
+do
+	echo ---- MALLOC_LIMIT=${i} -----
+	${CC} -dynamiclib -D MALLOC_LIMIT=${i} -o ${LIB_NAME} malloc_wrapper.c
+	DYLD_INSERT_LIBRARIES=${LIB_NAME} DYLD_FORCE_FLAT_NAMESPACE=1 ${BIN} 1 > /dev/null
+	if [ "$?" -gt "127" ]; then
+		echo -e "\033[31mKO\033[0m"
+		RETURN_VALUE=${FAILURE}
+	else
+		echo -e "\033[32mOK\033[0m"
+	fi
+	rm -f ${LIB_NAME}
+done
+exit ${RETURN_VALUE}
---
+++ malloc_wrapper.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <dlfcn.h>
+#include <execinfo.h>
+#include <stdbool.h>
+#include <strings.h>
+
+#ifndef MALLOC_LIMIT
+# define MALLOC_LIMIT 1
+#endif
+
+void *malloc(size_t size)
+{
+	static int i = 0;
+	static int subprocess = false;
+	static bool start_counter = false;
+	static void *(*real_malloc)(size_t);
+	void *called_func[2] = {NULL};
+	char **calledby;
+
+	if (subprocess == true)
+		return real_malloc(size);
+	if (real_malloc == NULL)
+		real_malloc = dlsym(RTLD_NEXT, "malloc");
+	if (subprocess == false)
+	{
+		subprocess = true;
+		backtrace(called_func, 2);
+		calledby = backtrace_symbols(called_func, 2);
+		subprocess = false;
+		if (calledby[1] != NULL)
+		{
+			if (strstr(calledby[1], "main") != NULL)
+				start_counter = true;
+			else
+				return real_malloc(size);
+		}
+	}
+	++i;
+	if (i <= MALLOC_LIMIT)
+		return real_malloc(size);
+	if (calledby[1] != NULL)
+	{
+		write(STDERR_FILENO, "Called by: ", 11);
+		write(STDERR_FILENO, calledby[1], strlen(calledby[1]));
+		write(STDERR_FILENO, "\n", 1);
+	}
+	return NULL;
+}
+
